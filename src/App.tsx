5e2ced9e1a661a3236ed83b60e245d1fc50d0bc2
Christa O <68796545+oshodic@users.noreply.github.com>
2023-12-17 21:24:52 -0500

    Added refactor changes
    Update state and relocate numOfFactors function to App.tsx. Refactor TextInput file

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import ContentContainer from "./ContentContainer";
+import primeFactorize from "./utils";
 
 export type Name = {first: string, last: string}
 
@@ -9,20 +10,25 @@ function App() {
     last: "",
   });
 
+  //I refactored and used the spread operator to create a copy of the previous state,
+  //then updated the field with the new name object.
   const handleNameUpdate=(field: keyof typeof name , newName: string) => {
-    setName((prevState) => {
-      prevState[field] = newName
-      return prevState
-    })
-  }
+    setName((prevState) => ({
+      ...prevState,
+      [field]: newName,
+    }));
+  };
+
+  //I removed the numOfFactors function from ContentContainer for better optimization.
+  const numOfFactors = useMemo(() => {
+    return primeFactorize(100000000000000000).length
+  }, []);
 
   return (
-   
       <div className="container">
         <h5>App</h5>
-        <ContentContainer handleNameUpdate={handleNameUpdate} name={name} />
+        <ContentContainer handleNameUpdate={handleNameUpdate} name={name} num={numOfFactors} />
       </div>
- 
   );
 }
 
--- src/TextInput.tsx
+++ src/TextInput.tsx
@@ -1,13 +1,44 @@
-const TextInput = ({ value, handleNameUpdate, name }: { name: string, value: string, handleNameUpdate:(field: "first" | "last", newName: string) => void } ) => {
+import React, { useCallback, useState } from "react";
+import { useDebouncedCallback } from "use-debounce";
+
+interface TextInputProps {
+  value: string;
+  handleNameUpdate: (field: "first" | "last", newName: string) => void;
+  name: string;
+};
+
+//I'm using useDebouncedCallback function to debounce the handleNameUpdate function.
+//The TextInput component will delay the execution of handleNameUpdate by 1 second and other components will not unnecessarily re-render.
+//I also added some state management to the input values so the TextInput component can better handle the debouncing.
+const TextInput: React.FC<TextInputProps> = ({value, handleNameUpdate, name}) => {
+  const [inputValue, setInputValue] = useState(name);
+
+  const debouncedHandleNameUpdate = useDebouncedCallback(
+    (newValue: string) => {
+      handleNameUpdate(value as "first" | "last", newValue);
+    },
+    1000
+  );
+
+  //useCallback function is used to make sure the function remains constant unless debouncedHandleNameUpdate is changed.
+  const handleChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const newValue = e.target.value;
+      setInputValue(newValue);
+      debouncedHandleNameUpdate(newValue);
+    },
+    [debouncedHandleNameUpdate]
+  );
+
   return (
     <div className="field">
       {`${value}: `}
       <input
-        value={name}
-        onChange={(e) => handleNameUpdate(value, e.target.value)}
+        value={inputValue}
+        onChange={handleChange}
       />
     </div>
-  );
+  )
 };
 
 export default TextInput
