1cc1c90fe81206c31ec51b420b6494c885c95a9d
Luis Alejo <lou.m.alejo@gmail.com>
2024-01-16 05:55:39 -0800

    adding comments

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,6 +3,8 @@ import primeFactorize from "../../lib/utils";
 
 export default function Footer() {
 
+  // As per the original tasks, this costly calculation is memoized.
+  // I've repurposed the result as the "Total Recipes count"
   const arbitrarilyImportantNumber = useMemo(() => primeFactorize(1000000).length, []);
 
   return (
