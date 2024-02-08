639fdbf27973c7cca85ddc5c2e88489fa91d496e
Amir Hackett <amirhackett@Amirs-MBP.lan>
2024-02-08 17:04:15 -0500

    completed the bonus task

--- src/FormContainer.tsx
+++ src/FormContainer.tsx
@@ -1,14 +1,15 @@
+import { useMemo } from 'react';
 import TextInput from './TextInput';
 import { Name } from './App';
 
 const FormContainer = ({handleNameUpdate, name}: {handleNameUpdate:(field: "first" | "last", newName: string) => void, name: Name }) => {
-  return (
+  return useMemo(() => (
     <div className="container">
       <h5>FormContainer</h5>
       <TextInput value="first" handleNameUpdate={handleNameUpdate} name={name.first} />
       <TextInput value="last" handleNameUpdate={handleNameUpdate} name={name.last}  />
     </div>
-  );
+  ), [handleNameUpdate, name.first, name.last]);
 }
 
-export default FormContainer
+export default FormContainer;  
