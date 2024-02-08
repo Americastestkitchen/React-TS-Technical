044d1af3da9a2ba5ce25ca0a337de41a31cba68e
joekgilberto <joekgilberto@gmail.com>
2024-02-08 11:43:28 -0500

    maps over trending recipes accounting for null userRatingsCount, updates text input to update state instead of modifying it, uses useMemo react hook as to not recall all prime numbers with every render, and adds handleSubmit function as to submit a new name once instead of rerendering with each character typed

--- src/FormContainer.tsx
+++ src/FormContainer.tsx
@@ -1,13 +1,29 @@
 import TextInput from './TextInput';
 import { Name } from './App';
+import { useState } from 'react';
 
-const FormContainer = ({handleNameUpdate, name}: {handleNameUpdate:(field: "first" | "last", newName: string) => void, name: Name }) => {
+const FormContainer = ({ handleNameUpdate, name }: { handleNameUpdate: (e: any, form: { first: string; last: string }) => void, name: Name }) => {
+
+  const [formData, setFormData] = useState({
+    first: "",
+    last: "",
+  })
+
+  const handleChange = (field: keyof typeof name, newName: string) => {
+    const updatedName = {
+      ...formData,
+      [field]: newName
+    }
+    setFormData(updatedName)
+  }
+
   return (
-    <div className="container">
+    <form className="container" onSubmit={(e) => handleNameUpdate(e, formData)}>
       <h5>FormContainer</h5>
-      <TextInput value="first" handleNameUpdate={handleNameUpdate} name={name.first} />
-      <TextInput value="last" handleNameUpdate={handleNameUpdate} name={name.last}  />
-    </div>
+      <TextInput value="first" handleChange={handleChange} name={formData.first} />
+      <TextInput value="last" handleChange={handleChange} name={formData.last} />
+      <button type='submit' aria-label='Submit'>Submit</button>
+    </form>
   );
 }
 
--- src/TextInput.tsx
+++ src/TextInput.tsx
@@ -1,12 +1,12 @@
 import { Field } from "./ContentContainer";
 
-const TextInput = ({ value, handleNameUpdate, name }: { name: string, value: Field, handleNameUpdate:(field: "first" | "last", newName: string) => void } ) => {
+const TextInput = ({ value, handleChange, name }: { name: string, value: Field, handleChange: (form: "first" | "last", newName: string) => void }) => {
   return (
     <div className="field">
       {`${value}: `}
       <input
         value={name}
-        onChange={(e) => handleNameUpdate(value, e.target.value)}
+        onChange={(e) => handleChange(value, e.target.value)}
       />
     </div>
   );
