f2126329903005c89c275fd4de3cced3d7686e97
Todd Garrison <toddlgarrison@gmail.com>
2024-02-28 16:36:00 -0500

    Remove comments

--- src/App.tsx
+++ src/App.tsx
@@ -13,11 +13,10 @@ function App() {
   const [trendingRecipes, setTrendingRecipes] = useState<Trending[]>()
 
   const handleNameUpdate=(field: keyof typeof name, newName: string) => {
-    setName((prevState) => {
-      const newState = {...prevState}
-      newState[field] = newName
-      return newState
-    })
+    setName(prevState => ({
+      ...prevState,
+      [field]: newName
+    }))
   }
 
   useEffect(() => {
@@ -32,10 +31,6 @@ function App() {
       <div className="container">
         <h5>App</h5>
         <ContentContainer handleNameUpdate={handleNameUpdate} name={name} />
-        {/* Render a component here that renders a list of trending items from the getTrending api function.
-        Display the title and userRatingsCount (Default the userRatingsCount to 0 if the field 
-          is null or missing in the api response) 
-         */}
         {trendingRecipes && <TrendingList trendingRecipes={trendingRecipes}/>}
       </div>
 
